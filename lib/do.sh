e7547984739c93884e9b86a881551a2a03f2e650
Edwin Kofler <edwin@kofler.dev>
2021-05-10 07:00:24 -0700

    feat: only copy over applicable commands

--- lib/do.sh
+++ lib/do.sh
@@ -4,16 +4,56 @@ doSync() {
 	command -v rsync &>/dev/null || {
 		die 'rsync not installed'
 	}
-
+	# TODO: cleanup
 	# source the configuration file
 	local glueFile="$WD/glue.sh"
 	helper_source_config "$glueFile" # exposes: languages
 
-	# TODO: only copy files relevant to current languages (and general ones)
-	for language in $languages; do
-		rsync -av --delete --progress --exclude '*-*-*' --include "*-$language-*" \
-			"$GLUE_STORE/" "$WD/.glue/"
+	# all applicable subcommands
+	local -a allFiles=()
+	readarray -d $'\0' allFiles < <(find "$GLUE_STORE/commands/auto" -ignore_readdir_race -mindepth 1 -maxdepth 1 -type f -printf "%f\0")
+	actualsubcommands=
+	for file in "${allFiles[@]}"; do
+		file="${file%%.*}"
+		file="${file%%-*}"
+
+		if ! [[ $actualsubcommands =~ ((^|\|)$file\|) ]]; then
+			actualsubcommands+="$file|"
+		fi
+
 	done
+	actualsubcommands="${actualsubcommands:: -1}"
+
+	# all applicable languages
+	for language in $languages; do
+		langRegex+="$language|"
+	done
+	langRegex="${langRegex:: -1}"
+
+
+	find "$WD/.glue/commands" -ignore_readdir_race -mindepth 1 -maxdepth 1 -type f -print0 | xargs -r0 rm
+	find "$GLUE_STORE/commands/auto" -ignore_readdir_race -mindepth 1 -maxdepth 1 -type f -regextype posix-egrep -regex "^.*/($actualsubcommands)?(-($langRegex))?(-(before|after))?\..*?$" -print0 | xargs -r0I '{}' cp '{}' "$WD/.glue/commands"
+
+	# find "$GLUE_STORE/commands/auto" -ignore_readdir_race -mindepth 1 -maxdepth 1 -type f -printf '%f\0' | perl -e "$(cat <<-"EOF"
+	# use strict;
+	# use warnings;
+
+	# my $input = do { local $/; <STDIN> };
+	# my @array = split('\0', $input);
+	# my $langRegex = "nodeapp|java";
+
+	# foreach(@array) {
+	# 	if ($_ =~ /(build|deploy)(-($langRegex))?(-before|-after)?\..*?$/) {
+	# 		print "$_\n";
+	# 	}
+	# }
+	# EOF
+	# )"
+
+	# for language in $languages; do
+	# 	rsync -acv --delete --progress --exclude '*-*-*' --include "*-$language-*" \
+	# 		"$GLUE_STORE/commands" "$WD/.glue/commands"
+	# done
 
 
 }
